58e55e046d5f3223c5dfda60429ed12af5e312ba
Natan <natanchaves47@gmail.com>
2025-09-18 00:26:54 -0300

    feat: Configuração do TailwindCSS e criação da estrutura do componente itemCarrinho

--- public/components/BoxCarrinho.tsx
+++ public/components/BoxCarrinho.tsx
@@ -3,11 +3,11 @@ import ItemCarrinho from "./ItemCarrinho";
 function BoxCarrinho() {
   return (
     <div
-      className="z-10 h-160 w-330 border-1 border-black rounded-2xl shadow-3xl flex justify-center items-center"
+      className="z-10 w-[1280px] max-w-[90%] h-[720px] p-6 border border-black rounded-2xl shadow-3xl flex flex-col items-center"
       style={{ backgroundColor: "#3A3A3A" }}
     >
       {/* Conteudo principal */}
-      <h1 className="absolute top-20 font-bold">Meu carrinho</h1>
+      <h1 className=" text-white text-4xl mb-15 font-bold">Meu carrinho</h1>
       <ItemCarrinho />
     </div>
   );
